5c805d0b11cc49ba4dab87f9b14fb0fcd7bd7ada
Nguyen Thanh <143480564+kenzoknz@users.noreply.github.com>
2025-04-29 22:11:58 +0700

    login form

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PBL3.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
