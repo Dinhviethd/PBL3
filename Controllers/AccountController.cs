e1793f0991a5d94a4c3f45dc0c6aaa8ed6c9288a
Dinhviethd <nguyentadinhviet210805@gmail.com>
2025-05-02 13:57:12 +0700

    Update Login

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using PBL3.Models;
+using PBL3.Models.ViewModel;
 
 namespace PBL3.Controllers
 {
@@ -20,9 +21,105 @@ namespace PBL3.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid Login Attempt.");
+            return View(model);
+        }
+        [HttpGet]
         public IActionResult VerifyEmail()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> VerifyEmail(VerifyEmailViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await _userManager.FindByNameAsync(model.Email);
+
+            if (user == null)
+            {
+                ModelState.AddModelError("", "User not found!");
+                return View(model);
+            }
+            else
+            {
+                return RedirectToAction("ChangePassword", "Account", new { username = user.UserName });
+            }
+        }
+
+        [HttpGet]
+        public IActionResult ChangePassword(string username)
+        {
+            if (string.IsNullOrEmpty(username))
+            {
+                return RedirectToAction("VerifyEmail", "Account");
+            }
+
+            return View(new ChangePasswordViewModel { Email = username });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Something went wrong");
+                return View(model);
+            }
+
+            var user = await _userManager.FindByNameAsync(model.Email);
+
+            if (user == null)
+            {
+                ModelState.AddModelError("", "User not found!");
+                return View(model);
+            }
+
+            var result = await _userManager.RemovePasswordAsync(user);
+            if (result.Succeeded)
+            {
+                result = await _userManager.AddPasswordAsync(user, model.NewPassword);
+                return RedirectToAction("Login", "Account");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- Models/ViewModel/ChangePasswordViewModel.cs
+++ Models/ViewModel/ChangePasswordViewModel.cs
@@ -12,13 +12,13 @@ namespace PBL3.Models.ViewModel
         [Required (ErrorMessage ="Mật khẩu là bắt buộc")]
         [DataType(DataType.Password)]
         [Display(Name = "Mật khẩu mới")]
-        public string newPassword {get; set;}
+        public string NewPassword {get; set;}
 
         [Required (ErrorMessage ="Mật Khẩu xác thực là bắt buộc")]
         [Compare("ConfirmNewPassword", ErrorMessage = "Password không khớp")]
         [DataType(DataType.Password)]
         [Display(Name = "Xác thực mật khẩu mới")]
-        public string confirmNewPassword { get; set; }
+        public string ConfirmNewPassword { get; set; }
 
     }
 }
